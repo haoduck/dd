9646a353afc4db55e604a6a28aab8cca3c6d5754
16406 <you@example.com>
2022-08-18 04:29:19 +0800

    GATEWAYIP

--- dd.sh
+++ dd.sh
@@ -7,7 +7,7 @@
 GET_NETCMD(){
     if [[ $static == 'true' ]];then
         MAINIP=$(ip route get 1 | awk -F 'src ' '{print $2}' | awk '{print $1}')
-        GATEWAYIP=$(ip route | grep default | grep onlink | awk '{print $3}')
+        GATEWAYIP=$(ip route | grep default | awk '{print $3}')
         SUBNET=$(ip -o -f inet addr show | awk '/scope global/{sub(/[^.]+\//,"0/",$4);print $4}' | head -1 | awk -F '/' '{print $2}')
         value=$(( 0xffffffff ^ ((1 << (32 - $SUBNET)) - 1) ))
         NETMASK="$(( (value >> 24) & 0xff )).$(( (value >> 16) & 0xff )).$(( (value >> 8) & 0xff )).$(( value & 0xff ))"
